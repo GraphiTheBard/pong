99cef5d5ce24a6c97bb16182081429ff83219d82
GraphiTheBard <aswin01010@gmail.com>
2022-12-24 14:03:50 +0530

    ball

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+
 namespace pong
 {
     class Program
@@ -8,8 +10,8 @@ namespace pong
         {
 
             Console.Clear();
-            const int playAreaLenth = 70, playAreaWidth = 28;
-            const char tile = '*';
+            const int playAreaLength = 75, playAreaWidth = 25;
+            const char tile = '=';
 
             const int paddleLength = 4;
             const char paddleBody = '|';
@@ -17,7 +19,15 @@ namespace pong
             int rightPaddlePos = 0;
             int leftPaddlePos = 0;
 
-            string playAreaBorder = string.Concat(Enumerable.Repeat(tile, playAreaLenth));
+            string playAreaBorder = string.Concat(Enumerable.Repeat(tile, playAreaLength));
+
+            int ballX = playAreaLength / 2;
+            int ballY = playAreaWidth / 2;
+            const char ball = 'o';
+
+
+            bool ballUp = true;
+            bool ballRight = true;
 
             while (true)
             {
@@ -28,15 +38,105 @@ namespace pong
                 Console.SetCursorPosition(0, playAreaWidth);
                 Console.WriteLine(playAreaBorder);
 
+
+                
+
+
                 for (int i = 0; i < paddleLength; i++)
                 {
                     Console.SetCursorPosition(0, 1 + i + leftPaddlePos) ;
                     Console.WriteLine(paddleBody);
 
-                    Console.SetCursorPosition(playAreaLenth - 1, 1 + i + rightPaddlePos);
+                    Console.SetCursorPosition(playAreaLength - 1, 1 + i + rightPaddlePos);
                     Console.WriteLine(paddleBody);
 
                 }
+
+
+                while (!Console.KeyAvailable)
+                {
+
+                    Console.SetCursorPosition(ballX, ballY);
+                    Console.WriteLine(ball);
+
+
+                    Thread.Sleep(70);
+
+
+                    if (ballUp)
+                    {
+                        ballY--;
+                    }
+
+                    else
+                    {
+                        ballY++;
+                    }
+
+                    if (ballY == 1 || ballY == playAreaWidth -1) {
+                        ballUp = !ballUp;
+                    
+                    }
+
+                   
+
+
+                    if (ballRight)
+                    {
+                        ballX++;
+
+                    }
+
+                    else {
+                        ballX--;
+                    }
+
+                    if (ballX < 1)
+                    {
+
+                        if (ballY >= leftPaddlePos + 1 || ballY <= leftPaddlePos + paddleLength)
+                        {
+                            ballRight = !ballRight;
+                        }
+
+                        else {
+                             ballX = playAreaLength / 2;
+                             ballY = playAreaWidth / 2;
+
+                        }
+                    }
+
+
+                    if (ballX == playAreaLength -1)
+                    {
+
+                        if (ballY >= rightPaddlePos + 1 || ballY <= rightPaddlePos + paddleLength)
+                        {
+                            ballRight = !ballRight;
+                        }
+
+                        else
+                        {
+                            ballX = playAreaLength / 2;
+                            ballY = playAreaWidth / 2;
+
+                        }
+                    }
+
+
+                    // Console.SetCursorPosition(ballX-1, ballY-1);
+                    //Console.WriteLine(' ');
+
+
+
+
+
+
+                }
+
+
+               
+
 
 
                 switch (Console.ReadKey().Key)
@@ -59,7 +159,7 @@ namespace pong
 
                     case ConsoleKey.UpArrow:
 
-                        if (leftPaddlePos > 0)
+                        if (rightPaddlePos > 0)
                         {
                             rightPaddlePos--;
                         }
@@ -67,7 +167,7 @@ namespace pong
 
                     case ConsoleKey.DownArrow:
 
-                        if (leftPaddlePos < playAreaWidth - paddleLength - 1)
+                        if (rightPaddlePos < playAreaWidth - paddleLength - 1)
                         {
                             rightPaddlePos++;
                         }
@@ -75,6 +175,16 @@ namespace pong
 
 
 
+
+                }
+
+                for (int i = 1; i < playAreaWidth; i++)
+                {
+                    Console.SetCursorPosition(0, i);
+                    Console.WriteLine(' ');
+
+                    Console.SetCursorPosition(playAreaLength - 1, i);
+                    Console.WriteLine(' ');
 
                 }
             }
