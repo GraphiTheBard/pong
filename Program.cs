7229f76c9029f1698e2360c4338dac44c43317a3
GraphiTheBard <aswin01010@gmail.com>
2022-12-24 15:10:56 +0530

    everthing is epic cool

--- Program.cs
+++ Program.cs
@@ -10,28 +10,51 @@ namespace pong
         {
 
             Console.Clear();
+
+
+            //play area
             const int playAreaLength = 75, playAreaWidth = 25;
             const char tile = '=';
+            string playAreaBorder = string.Concat(Enumerable.Repeat(tile, playAreaLength));
 
+            //paddle and player scores
             const int paddleLength = 4;
             const char paddleBody = '|';
 
-            int rightPaddlePos = 0;
-            int leftPaddlePos = 0;
+            int rightPaddlePos = playAreaWidth / 2 - 1;
+            int leftPaddlePos = playAreaWidth / 2 - 1;
 
-            string playAreaBorder = string.Concat(Enumerable.Repeat(tile, playAreaLength));
+            int leftPlayerScore = 0;
+            int rightPlayerScore = 0;
+
+
+
+            //ball 
 
             int ballX = playAreaLength / 2;
             int ballY = playAreaWidth / 2;
             const char ball = 'o';
 
+            int ballSpeedModifier = 90;
+
 
             bool ballUp = true;
             bool ballRight = true;
 
+
+            //Display instructions
+
+            Console.SetCursorPosition(playAreaLength + 10, playAreaWidth / 2 - 1);
+            Console.WriteLine("R: reset");
+            Console.SetCursorPosition(playAreaLength + 10, playAreaWidth / 2);
+            Console.WriteLine("W/S: left paddle");
+            Console.SetCursorPosition(playAreaLength + 10, playAreaWidth / 2 + 1);
+            Console.WriteLine("UP/DOWN: right paddle");
+
+            //Main thingy    
             while (true)
             {
-
+                //setting up the play area
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine(playAreaBorder);
 
@@ -39,12 +62,12 @@ namespace pong
                 Console.WriteLine(playAreaBorder);
 
 
-                
 
 
+                //drawing/updating player position
                 for (int i = 0; i < paddleLength; i++)
                 {
-                    Console.SetCursorPosition(0, 1 + i + leftPaddlePos) ;
+                    Console.SetCursorPosition(0, 1 + i + leftPaddlePos);
                     Console.WriteLine(paddleBody);
 
                     Console.SetCursorPosition(playAreaLength - 1, 1 + i + rightPaddlePos);
@@ -52,7 +75,7 @@ namespace pong
 
                 }
 
-
+                //ball movement
                 while (!Console.KeyAvailable)
                 {
 
@@ -60,7 +83,10 @@ namespace pong
                     Console.WriteLine(ball);
 
 
-                    Thread.Sleep(70);
+                    Thread.Sleep(ballSpeedModifier);
+
+                    Console.SetCursorPosition(ballX, ballY);
+                    Console.WriteLine(' ');
 
 
                     if (ballUp)
@@ -73,12 +99,13 @@ namespace pong
                         ballY++;
                     }
 
-                    if (ballY == 1 || ballY == playAreaWidth -1) {
+                    if (ballY == 1 || ballY == playAreaWidth - 1)
+                    {
                         ballUp = !ballUp;
-                    
+
                     }
 
-                   
+
 
 
                     if (ballRight)
@@ -87,30 +114,15 @@ namespace pong
 
                     }
 
-                    else {
+                    else
+                    {
                         ballX--;
                     }
 
-                    if (ballX < 1)
+                    if (ballX == 1)
                     {
 
-                        if (ballY >= leftPaddlePos + 1 || ballY <= leftPaddlePos + paddleLength)
-                        {
-                            ballRight = !ballRight;
-                        }
-
-                        else {
-                             ballX = playAreaLength / 2;
-                             ballY = playAreaWidth / 2;
-
-                        }
-                    }
-
-
-                    if (ballX == playAreaLength -1)
-                    {
-
-                        if (ballY >= rightPaddlePos + 1 || ballY <= rightPaddlePos + paddleLength)
+                        if (ballY >= leftPaddlePos + 1 && ballY <= leftPaddlePos + paddleLength)
                         {
                             ballRight = !ballRight;
                         }
@@ -119,13 +131,33 @@ namespace pong
                         {
                             ballX = playAreaLength / 2;
                             ballY = playAreaWidth / 2;
+                            rightPlayerScore++;
 
                         }
                     }
 
 
-                    // Console.SetCursorPosition(ballX-1, ballY-1);
-                    //Console.WriteLine(' ');
+                    if (ballX == playAreaLength - 2)
+                    {
+
+                        if (ballY >= rightPaddlePos + 1 && ballY <= rightPaddlePos + paddleLength)
+                        {
+                            ballRight = !ballRight;
+
+                        }
+
+                        else
+                        {
+                            ballX = playAreaLength / 2;
+                            ballY = playAreaWidth / 2;
+                            leftPlayerScore++;
+
+                        }
+                    }
+
+
+                    Console.SetCursorPosition(playAreaLength / 2, playAreaWidth + 2);
+                    Console.WriteLine(leftPlayerScore + "|" + rightPlayerScore);
 
 
 
@@ -135,10 +167,10 @@ namespace pong
                 }
 
 
-               
 
 
 
+                //reading player input
                 switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.W:
@@ -174,10 +206,22 @@ namespace pong
                         break;
 
 
+                    case ConsoleKey.R:
+
+                        leftPlayerScore = 0;
+                        rightPlayerScore = 0;
+
+                        ballX = playAreaLength / 2;
+                        ballY = playAreaWidth / 2;
+
+                        break;
+
+
 
 
                 }
 
+                //deleting old player position
                 for (int i = 1; i < playAreaWidth; i++)
                 {
                     Console.SetCursorPosition(0, i);
@@ -187,18 +231,11 @@ namespace pong
                     Console.WriteLine(' ');
 
                 }
+
+
+
+
             }
-
-
-
-            //player movement
-
-            
-
-
-
-
-
 
         }
     }
